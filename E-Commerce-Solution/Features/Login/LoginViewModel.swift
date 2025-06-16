57fea2c367c97e14fc044b8f86e59e4f9f3d7ccd
Pawan Sharma <sharma.pawan@yash.com>
2025-06-16 14:07:16 +0530

    Handle error

--- E-Commerce-Solution/Features/Login/LoginViewModel.swift
+++ E-Commerce-Solution/Features/Login/LoginViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Models
+import ValidationKit
 import Foundation
 
 struct LoginViewModel {
@@ -17,9 +18,12 @@ struct LoginViewModel {
     do {
       let _email = try EmailAddress(email)
       self.email = _email
-    } catch let result {
+    } catch let error as Validator.EmailValidationResult {
       // TODO: Handle the error here for email validation failure
+      print(error.localizedDescription)
       self.email = nil
+    } catch let error {
+      print("Unknown Error: ", error.localizedDescription)
     }
   }
 
@@ -27,9 +31,11 @@ struct LoginViewModel {
     do {
       let _name = try Name(name)
       self.name = _name
-    } catch let result {
+    } catch let error as Validator.NameValidationResult {
       // TODO: Handle the error here for Name validation failure
-      self.name = nil
+      print(error.localizedDescription)
+    } catch let error {
+      print("Unknown Error: ", error.localizedDescription)
     }
   }
 
@@ -37,9 +43,12 @@ struct LoginViewModel {
     do {
       let _mobileNumber = try MobileNumber(mobileNumber)
       self.mobileNumber = _mobileNumber
-    } catch let result {
-      // TODO: Handle the error here for mobile number validation failure
+    } catch let error as Validator.MobileNumberValidationResult {
       self.mobileNumber = nil
+      print(error.localizedDescription)
+    } catch let error {
+      // TODO: Handle the error here for mobile number validation failure
+      print("Unknown Error: ", error.localizedDescription)
     }
   }
 }
