6c4c5d0a4a3f9b1541109e8db9891ba2b24b3098
Pawan Sharma <sharma.pawan@yash.com>
2025-06-17 17:12:02 +0530

    Show LoginvView

--- E-Commerce-Solution/E_Commerce_SolutionApp.swift
+++ E-Commerce-Solution/E_Commerce_SolutionApp.swift
@@ -13,8 +13,7 @@ struct E_Commerce_SolutionApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            LoginView()
         }
     }
 }
