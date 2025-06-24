b2e0e8c0c8b2ef5931494417210bee6e7503ea77
Pawan Sharma <sharma.pawan@yash.com>
2025-06-24 12:06:27 +0530

    Enable Firebase Auth with email and Google sign in

--- E-Commerce-Solution/App/AppDelegate.swift
+++ E-Commerce-Solution/App/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 }
